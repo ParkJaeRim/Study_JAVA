ed4dc4550e72ad19b64e77a6347bbe78ff4138fb
jaeriiimy <riim715@naver.com>
2020-01-15 23:48:37 +0900

    Create boj_1001.java

---
+++ boj_1001.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class boj_1001 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		scanner.close();
+		
+		System.out.println(a-b);
+
+	}
+
+}
