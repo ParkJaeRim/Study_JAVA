dc3c34759c652c1ac3c6fd22efe45122d3b3cff5
jaeriiimy <riim715@naver.com>
2020-01-15 23:59:05 +0900

    Create boj_10869.java

---
+++ boj_10869.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class boj_10869 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		scanner.close();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+
+	}
+
+}
