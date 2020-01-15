97ade16e6c7d64bb68e39f1237d14275181179c1
jaeriiimy <riim715@naver.com>
2020-01-15 23:48:33 +0900

    Create boj_1008.java

---
+++ boj_1008.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class boj_1008 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		scanner.close();
+		double c = (double)a / b ;
+				
+		System.out.println(c);
+
+	}
+
+}
